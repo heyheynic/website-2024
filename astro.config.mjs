e5f50faca143f2c4a6882971862ce2c25ae66ab2
heyheynic <nicsayhey@gmail.com>
2024-09-25 19:33:20 +0200

    push for tonight, working on the interactive resume, specifically sec2 and 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://nicsay.dev"
+  site: "https://nicsay.dev",
+  integrations: [icon()]
 });
